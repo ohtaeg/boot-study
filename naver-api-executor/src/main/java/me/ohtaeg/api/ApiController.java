d766215efb441e3fb8dc9f0433d1eb45747b13a4
ohtaeg <otk1090@naver.com>
2020-01-14 23:46:19 +0900

    refactor (blog controller) : blog request 받도록 변경

--- naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
+++ naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
@@ -1,13 +1,26 @@
 package me.ohtaeg.api;
 
+import me.ohtaeg.api.dto.BlogRequest;
+import me.ohtaeg.application.SearchService;
+import me.ohtaeg.domain.response.Blog;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping("/")
-    public String defaultUrl() {
-        return "hello world, please rewrite /blog + /{query}";
+    private SearchService searchService;
+
+    public ApiController(final SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/blog")
+    public Blog blog(@Valid BlogRequest blogRequest) {
+        return (Blog) searchService.search(blogRequest);
     }
 }
