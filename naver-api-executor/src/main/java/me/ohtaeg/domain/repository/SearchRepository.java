43807e6c500d42ec377261ee29de1d7dc76dd4ac
ohtaeg <otk1090@naver.com>
2020-01-16 01:13:56 +0900

    refactor (remove todo) : TODO 제거

--- naver-api-executor/src/main/java/me/ohtaeg/domain/repository/SearchRepository.java
+++ naver-api-executor/src/main/java/me/ohtaeg/domain/repository/SearchRepository.java
@@ -1,14 +1,12 @@
 package me.ohtaeg.domain.repository;
 
-import me.ohtaeg.domain.response.OpenApi;
+import me.ohtaeg.domain.response.SearchApi;
 import org.springframework.http.HttpHeaders;
 
 public interface SearchRepository {
-    String UTF_8 = "UTF-8";
 
-    OpenApi search(OpenApi openApi);
+    SearchApi search(SearchApi searchApi);
 
-    // TODO : refactor - repo interface가 header에 대한 default 메서드를 갖고있는게 맞을까?
     default HttpHeaders getHeader(String id, String secret) {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("X-Naver-Client-Id", id);
