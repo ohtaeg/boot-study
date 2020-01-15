c5ad7c42bd55c7d1318beab7164aec1fc10e74e2
ohtaeg <otk1090@naver.com>
2020-01-16 02:27:26 +0900

    feat (blog + movie search) : blog, movie 동시 조회 기능 추가

--- naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
+++ naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
@@ -4,14 +4,17 @@ import me.ohtaeg.api.dto.BlogRequest;
 import me.ohtaeg.api.dto.MovieRequest;
 import me.ohtaeg.application.BlogService;
 import me.ohtaeg.application.MovieService;
-import me.ohtaeg.application.SearchService;
 import me.ohtaeg.domain.response.Blog;
 import me.ohtaeg.domain.response.Movie;
+import me.ohtaeg.domain.response.SearchApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -33,5 +36,16 @@ public class ApiController {
     @GetMapping("/movie")
     public Movie movie(@Valid MovieRequest movieRequest) {
         return (Movie) movieService.search(movieRequest);
+    }
+
+    @GetMapping("/with")
+    public List<SearchApi> with(@RequestParam(required = true) String query) {
+        List<SearchApi> searchApis = new ArrayList<>();
+        Blog blog = (Blog) blogService.search(new BlogRequest(query));
+        Movie movie = (Movie) movieService.search(new MovieRequest(query));
+
+        searchApis.add(blog);
+        searchApis.add(movie);
+        return searchApis;
     }
 }
