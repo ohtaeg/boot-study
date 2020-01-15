32c5357ef49e97c05de5b5a0c0a98e9be1a7cafd
ohtaeg <otk1090@naver.com>
2020-01-16 02:07:04 +0900

    feat (movie) : 영화 검색 추가

--- naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
+++ naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
@@ -1,8 +1,12 @@
 package me.ohtaeg.api;
 
 import me.ohtaeg.api.dto.BlogRequest;
+import me.ohtaeg.api.dto.MovieRequest;
+import me.ohtaeg.application.BlogService;
+import me.ohtaeg.application.MovieService;
 import me.ohtaeg.application.SearchService;
 import me.ohtaeg.domain.response.Blog;
+import me.ohtaeg.domain.response.Movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +17,21 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class ApiController {
 
-    private SearchService searchService;
+    private BlogService blogService;
+    private MovieService movieService;
 
-    public ApiController(final SearchService searchService) {
-        this.searchService = searchService;
+    public ApiController(final BlogService blogService, final MovieService movieService) {
+        this.blogService = blogService;
+        this.movieService = movieService;
     }
 
     @GetMapping("/blog")
     public Blog blog(@Valid BlogRequest blogRequest) {
-        return (Blog) searchService.search(blogRequest);
+        return (Blog) blogService.search(blogRequest);
+    }
+
+    @GetMapping("/movie")
+    public Movie movie(@Valid MovieRequest movieRequest) {
+        return (Movie) movieService.search(movieRequest);
     }
 }
