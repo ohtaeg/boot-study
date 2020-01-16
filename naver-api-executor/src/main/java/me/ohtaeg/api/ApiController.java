eab996285d760bb467230027069452faa65846a9
ohtaeg <otk1090@naver.com>
2020-01-16 18:58:05 +0900

    refactor (casting remove) : 서비스에서 캐스팅하도록 변경

--- naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
+++ naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
@@ -2,6 +2,7 @@ package me.ohtaeg.api;
 
 import me.ohtaeg.api.dto.BlogRequest;
 import me.ohtaeg.api.dto.MovieRequest;
+import me.ohtaeg.api.dto.SearchWord;
 import me.ohtaeg.application.BlogService;
 import me.ohtaeg.application.MovieService;
 import me.ohtaeg.domain.response.Blog;
@@ -9,7 +10,6 @@ import me.ohtaeg.domain.response.Movie;
 import me.ohtaeg.domain.response.SearchApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -30,22 +30,36 @@ public class ApiController {
 
     @GetMapping("/blog")
     public Blog blog(@Valid BlogRequest blogRequest) {
-        return (Blog) blogService.search(blogRequest);
+        return blogService.search(blogRequest);
     }
 
     @GetMapping("/movie")
     public Movie movie(@Valid MovieRequest movieRequest) {
-        return (Movie) movieService.search(movieRequest);
+        return movieService.search(movieRequest);
     }
 
     @GetMapping("/with")
-    public List<SearchApi> with(@RequestParam(required = true) String query) {
+    public List<SearchApi> with(@Valid SearchWord searchWord) {
+        // TODO : refactoring
         List<SearchApi> searchApis = new ArrayList<>();
-        Blog blog = (Blog) blogService.search(new BlogRequest(query));
-        Movie movie = (Movie) movieService.search(new MovieRequest(query));
+        String query = searchWord.getQuery();
+        int start = searchWord.getStart();
+        int display = searchWord.getDisplay();
+
+        BlogRequest blogRequest = new BlogRequest(query);
+        blogRequest.setStart(start);
+        blogRequest.setDisplay(display);
+
+        MovieRequest movieRequest = new MovieRequest(query);
+        movieRequest.setStart(start);
+        movieRequest.setDisplay(display);
+
+        Blog blog = blogService.search(blogRequest);
+        Movie movie = movieService.search(movieRequest);
 
         searchApis.add(blog);
         searchApis.add(movie);
+
         return searchApis;
     }
 }
