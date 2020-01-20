58e56bc577dbdbfbf01260437229f3d75241b90f
ohtaeg <otk1090@naver.com>
2020-01-20 14:56:49 +0900

    refactor (change was) : 언더토우로 변경 및 실행중인 포트번호 구해온다.

--- naver-api-executor/src/main/java/me/ohtaeg/AppRunner.java
+++ naver-api-executor/src/main/java/me/ohtaeg/AppRunner.java
@@ -5,11 +5,14 @@ import me.ohtaeg.config.SomeThing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class AppRunner implements ApplicationRunner {
+public class AppRunner implements ApplicationRunner, ApplicationListener<ServletWebServerInitializedEvent> {
 
     @Autowired
     NaverApiProperties naverApiProperties;
@@ -18,8 +21,14 @@ public class AppRunner implements ApplicationRunner {
     SomeThing someThing;
 
     @Override
-    public void run(final ApplicationArguments args) throws Exception {
-        System.out.println(naverApiProperties.getClientId());
-        System.out.println(someThing.getName());
+    public void run(final ApplicationArguments args) {
+        System.out.println("clientId : " + naverApiProperties.getClientId());
+        System.out.println("test something : " + someThing.getName());
+    }
+
+    @Override
+    public void onApplicationEvent(final ServletWebServerInitializedEvent event) {
+        ServletWebServerApplicationContext applicationContext = event.getApplicationContext();
+        System.out.println("port : " + applicationContext.getWebServer().getPort());
     }
 }
